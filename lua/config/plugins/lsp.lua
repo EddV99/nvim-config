d824bbdad2572a4bc9f25737e17e1a6b31358c5b
Eduardo Valdivia <eddval0808@gmail.com>
2025-03-14 22:55:54 -0600

    ensure used lsps are installed and added typescript lsp

--- lua/config/plugins/lsp.lua
+++ lua/config/plugins/lsp.lua
@@ -20,11 +20,16 @@ return {
     lazy = false,
     config = function()
       require("mason").setup()
-      require("mason-lspconfig").setup()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "lua_ls", "ts_ls" },
+        automatic_installation = false,
+        handlers = nil,
+      })
 
       local capabilities = require('blink.cmp').get_lsp_capabilities()
       local lspconfig = require("lspconfig")
       lspconfig.lua_ls.setup({ capabilities = capabilities, })
+      lspconfig.ts_ls.setup({ capabilities = capabilities, })
 
       vim.api.nvim_create_autocmd('LspAttach', {
         callback = function(args)
