959fd4031bcaf3eb2eb8c67c82f5eacf07dba975
Eduardo Valdivia <eddval0808@gmail.com>
2025-03-15 11:17:28 -0600

    formatting

--- lua/config/plugins/colorscheme.lua
+++ lua/config/plugins/colorscheme.lua
@@ -4,14 +4,12 @@ return {
     lazy = false,
     enabled = true,
     priority = 1000,
-    -- what the heck waltuh
     config = function()
       require("nightfox").setup({
         options = {
           transparent = true,
         }
       })
-
       vim.cmd.colorscheme("carbonfox")
     end,
   }
