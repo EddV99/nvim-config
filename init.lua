4b9e90d2987fbebe46b788c3650ca88ae0254427
Eduardo Valdivia <eddval0808@gmail.com>
2025-04-09 19:12:02 -0600

    cool keybind

--- init.lua
+++ init.lua
@@ -51,5 +51,7 @@ vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 vim.keymap.set("n", "<leader>fs", vim.cmd.w)
 vim.keymap.set("x", "<leader>p", "\"_dP")
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 require("config.lazy")
