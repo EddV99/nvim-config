c937a95860afa0bad56dfc894562a92b24c4c94e
Eduardo Valdivia <eddval0808@gmail.com>
2025-11-10 20:54:25 -0700

    update mason lua file

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,4 +1,15 @@
-vim.pack.add({'https://github.com/mason-org/mason.nvim'})
+vim.pack.add({ 'https://github.com/mason-org/mason.nvim' })
 
 local mason = require('mason')
 mason.setup()
+
+local registry = require('mason-registry')
+
+local to_install = { 'gopls', 'lua-language-server', 'delve'}
+
+for _, package_name in ipairs(to_install) do
+  if not registry.is_installed(package_name) then
+    local package = registry.get_package(package_name)
+    package:install()
+  end
+end
